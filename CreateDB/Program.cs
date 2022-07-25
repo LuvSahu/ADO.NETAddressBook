d1c503aef53564ac1a6653e03fbef9c94ffe2812
Luv sahu <lksahu404@gmail.com>
2022-07-25 17:47:15 +0530

    [Luv SAHU] This code for create Data Base

---
+++ CreateDB/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ADO.NETAddressBook;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to AddressBook ADO.NET!");
+        Console.WriteLine("Select option\n1)Create AddrssBookServiceDatabase");
+        int op = Convert.ToInt16(Console.ReadLine());
+        AddressBook addressBook= new AddressBook();
+
+        switch (op)
+        {
+            case 1:
+                addressBook.Create_Database();
+                break;
+
+        }
+    }
+}
