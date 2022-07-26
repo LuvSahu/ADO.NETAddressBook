2440eb09507a833abdedb584ebd68e8eb80a72d6
Luv sahu <lksahu404@gmail.com>
2022-07-26 10:40:40 +0530

    [Luv SAHU] This code for create Table

---
+++ CreateTable/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ADO.NETAddressBook;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Select option\n1.Create AddrssBookServiceDatabase\n2.CreateTable");
+        int op = Convert.ToInt16(Console.ReadLine());
+        AddressBook addressBook = new AddressBook();
+
+        switch (op)
+        {
+            case 1:
+                addressBook.Create_Database();
+                break;
+            case 2:
+                addressBook.CreateTable();
+                break;
+
+        }
+    }
+}
