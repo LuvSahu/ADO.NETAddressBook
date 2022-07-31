d3730e6e1b6dd37c6fecf3f29cf17bf017b5c34a
Luv sahu <lksahu404@gmail.com>
2022-07-31 18:02:29 +0530

    [Luv SAHU] Implemented the add contacts using rest sharp

---
+++ RestSharpAddedContacts/CreateDB.sql
@@ -0,0 +1,13 @@
+CREATE PROCEDURE SpAddressBook
+@FirstName nvarchar(200),
+@LastName  nvarchar(200),
+@Address nvarchar(200),
+@City nvarchar(200),
+@State nvarchar(200),
+@Zip bigint,
+@Email nvarchar(200),
+@PhoneNumber bigint
+AS
+insert into Addressbook(FirstName,LastName,Address,City,State,Zip,Email,PhoneNumber) values(@FirstName,@LastName,@Address,@City,@State,@Zip,@Email,@PhoneNumber);
+
+select * from AddressBook;
